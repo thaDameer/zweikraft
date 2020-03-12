2ebefc7fccc9ddf2a14b7a7d23ec51364ae8b2d0
thaDameer <thaDameer@users.noreply.github.com>
2020-03-12 20:47:29 +0100

    Fabio

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public static CameraScript instance;
+    public Camera mainCamera; 
+
+    private void Awake() 
+    {
+        if(instance != null)
+        {
+            instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/FabianController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FabianController : MonoBehaviour
+{
+    public float moveSpeed;
+    Vector2 input;
+    Rigidbody myRb;
+    Camera mainCam;
+    Vector3 moveDir;
+
+    private void Start()
+    {
+        myRb = transform.GetComponent<Rigidbody>();
+    }
+
+
+
+    void Update()
+    {     
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+       
+      //  myRb.MovePosition(moveDir * Time.deltaTime);
+       
+    }
+
+    private void FixedUpdate() 
+    {
+        myRb.velocity += new Vector3(input.x, myRb.velocity.y, input.y).normalized * moveSpeed * Time.fixedDeltaTime;        
+    }
+
+    
+
+    
+}
